87c1a502e63b5fa14c3c5be24923b0780e249752
Vinicius Moreira <vinicius.vsm777@gmail.com>
2024-03-05 14:50:59 -0300

    chore: estruturando migrações

---
+++ database/migrations/2024_03_05_171838_create_disciplines_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('disciplines', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->integer('total_hours');
+            $table->boolean('is_active');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('disciplines');
+    }
+};
---
+++ database/migrations/2024_03_05_171900_create_sections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('discipline_id');
+            $table->timestamp('initial_time');
+            $table->timestamp('final_time');
+            $table->boolean('is_active');
+
+            $table->foreign('discipline_id')->references('id')->on('disciplines');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections');
+    }
+};
