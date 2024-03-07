705ef8bcba97dad8504df656579f03f54d8b209d
Vinicius Moreira <vinicius.vsm777@gmail.com>
2024-03-07 13:29:52 -0300

    feat: login de usuários

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AuthRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function auth(AuthRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect']
+            ]);
+        }
+
+        // Logout other devices
+        // if($request->has('logout_others_devices'))
+        $user->tokens()->delete();
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Sucesso'
+        ]);
+    }
+
+    public function me(Request $request)
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'me' => $user
+        ]);
+    }
+
+    public function dashboard()
+    {
+        return response()->json([
+            'success' => true,
+            'message' => "Deu certo"
+        ]);
+
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware(['auth:sanctum']);
+Route::get('/me', [AuthController::class, 'me'])->middleware(['auth:sanctum']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard']);
+});
+
+Route::get('/', function (Request $request) {
+    return response()->json(['success' => true, 'message' => 'Hello World!']);
 });
